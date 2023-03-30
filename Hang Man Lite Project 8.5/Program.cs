38323ca0bf524aa443fb5cac928a9aa6a94836ca
362700239 <jennarbloor@gmail.com>
2023-03-30 10:59:22 -0400

    fix double letter probem

--- Hang Man Lite Project 8.5/Program.cs
+++ Hang Man Lite Project 8.5/Program.cs
@@ -9,18 +9,24 @@ namespace Hang_Man_Lite_Project_8._5
             bool done = false;
             bool done_ = false;
             Random generator = new Random();           
-            string displayWord;
+            string displayWord = "";
             string randomWord;
             int incorrectGuesses;
             string input;
-            string input_;
-            List<string> usedLetters = new List<string>() { "" };
-            displayWord = "--------";
-            List<string> words = new List<string>() {"POTATO", "TREEHOUSE", "GRASS"};
+            string input_;           
+            List<string> usedLetters = new List<string>() { "" };          
+            List<string> words = new List<string>() {"POTATO", "TREEHOUSE", "GRASS", "SANDWICH"};
             incorrectGuesses = 0;
             while (!done_)
             {
+                incorrectGuesses = 0;
+                displayWord = "";
+                usedLetters.Clear();
                 randomWord = (words[generator.Next(0, words.Count - 1)]);
+                for (int i = 0; i < randomWord.Length; i++)
+                {
+                    displayWord += "-";
+                }                
                 done = false;
                 Console.Clear();
                 while (!done)
@@ -28,52 +34,34 @@ namespace Hang_Man_Lite_Project_8._5
                     Console.WriteLine("-HangMan-Lite-Game-");
                     Console.WriteLine(" -----------------");
                     Console.WriteLine("");
-                    //make drawStickMan method to hide drawings in at the end
                     if (incorrectGuesses == 0)
                     {
-                        Console.WriteLine(" +---+");
-                        Console.WriteLine(" |   |");
-                        Console.WriteLine("     |");
-                        Console.WriteLine("     |");
-                        Console.WriteLine("     |");
-                        Console.WriteLine("     |");
-                        Console.WriteLine(" =========");
+                        DrawStickMen(0);
                     }
                     else if (incorrectGuesses == 1)
                     {
-                        Console.WriteLine(" +---+");
-                        Console.WriteLine(" |   |");
-                        Console.WriteLine(" O   |");
-                        Console.WriteLine("     |");
-                        Console.WriteLine("     |");
-                        Console.WriteLine("     |");
-                        Console.WriteLine(" =========");
+                        DrawStickMen(1);
                     }
                     else if (incorrectGuesses == 2)
                     {
-                        Console.WriteLine(" +---+");
-                        Console.WriteLine(" |   |");
-                        Console.WriteLine(" O   |");
-                        Console.WriteLine("/|\\  |");
-                        Console.WriteLine("     |");
-                        Console.WriteLine("     |");
-                        Console.WriteLine(" =========");
+                        DrawStickMen(2);
                     }
                     else if (incorrectGuesses == 3)
                     {
-                        Console.WriteLine(" +---+");
-                        Console.WriteLine(" |   |");
-                        Console.WriteLine(" O   |");
-                        Console.WriteLine("/|\\  |");
-                        Console.WriteLine("/ \\  |");
-                        Console.WriteLine("     |");
-                        Console.WriteLine(" =========");
+                        DrawStickMen(3);
+                    }
+                    else if (incorrectGuesses == 4)
+                    {
+                        DrawStickMen(4);
+                        Console.WriteLine("Oh no you lost!");                      
+                        Console.ReadLine();                       
+                        done = true;
                     }
                     Console.WriteLine("");
                     Console.WriteLine("(If you want to quit type /)");
                     Console.WriteLine("Guess a letter:");
                     Console.WriteLine(displayWord);
-                    input = Console.ReadLine().ToUpper();
+                    input = Console.ReadLine().ToUpper();                   
                     if (usedLetters.Contains(input))
                     {
                         Console.WriteLine("You already used that letter: Try Again");
@@ -88,11 +76,12 @@ namespace Hang_Man_Lite_Project_8._5
                         usedLetters.Add(input);
                     }
                     else
-                    {
-                        displayWord.Remove(randomWord.IndexOf(input), 1);
-                        displayWord.Insert(randomWord.IndexOf(input), input);
+                    {  
+                        displayWord = displayWord.Remove(randomWord.IndexOf(input), 1);
+                        displayWord = displayWord.Insert(randomWord.IndexOf(input), input);      
                         usedLetters.Add(input);
                     }
+                    
                     Console.WriteLine("");
                     Console.WriteLine("Press Enter to Continue:");
                     Console.ReadLine();
@@ -109,6 +98,59 @@ namespace Hang_Man_Lite_Project_8._5
                 {
                     done_ = true;
                 }
+            }
+        }
+        public static void DrawStickMen(int i)
+        {
+            if (i == 0)
+            {
+                Console.WriteLine(" +---+");
+                Console.WriteLine(" |   |");
+                Console.WriteLine("     |");
+                Console.WriteLine("     |");
+                Console.WriteLine("     |");
+                Console.WriteLine("     |");
+                Console.WriteLine(" =========");
+            }
+            else if (i == 1)
+            {
+                Console.WriteLine(" +---+");
+                Console.WriteLine(" |   |");
+                Console.WriteLine(" O   |");
+                Console.WriteLine("     |");
+                Console.WriteLine("     |");
+                Console.WriteLine("     |");
+                Console.WriteLine(" =========");
+            }
+            else if (i == 2)
+            {
+                Console.WriteLine(" +---+");
+                Console.WriteLine(" |   |");
+                Console.WriteLine(" O   |");
+                Console.WriteLine("/|\\  |");
+                Console.WriteLine("     |");
+                Console.WriteLine("     |");
+                Console.WriteLine(" =========");
+            }
+            else if (i == 3)
+            {
+                Console.WriteLine(" +---+");
+                Console.WriteLine(" |   |");
+                Console.WriteLine(" O   |");
+                Console.WriteLine("/|\\  |");
+                Console.WriteLine("/ \\  |");
+                Console.WriteLine("     |");
+                Console.WriteLine(" =========");
+            }
+            else if (i == 4) 
+            { 
+                Console.WriteLine(" +---+");
+                Console.WriteLine("     |");
+                Console.WriteLine("     |");
+                Console.WriteLine("\\O/  |");
+                Console.WriteLine(" |   |");
+                Console.WriteLine("/ \\  |");
+                Console.WriteLine(" =========");
             }
         }
     }
